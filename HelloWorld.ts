7d60875a061f5ff3a6fa7af9c8c5b995d0a25b2a
Brendan Nelson <brendan.nelson49@gmail.com>
2020-02-03 17:31:09 -0800

    final commit for lab assignment 1

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,12 +1,17 @@
 class HelloWorld {
-    constructor{
 
-    }
-    greet() {
-        
+    firstName = '';
+    lastName: string;
+
+    constructor(firstName:string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
-    showGreet() {
-        console.log(Hello)
+    sayHello(){
+        console.log('Hello World!' + ' My name is ' + this.firstName + ' ' + this.lastName);
     }
 }
+
+const myHelloClassInstance = new HelloWorld('Brendan', 'Nelson');
+myHelloClassInstance.sayHello();
